da3f979cf86760276497f7ef9c0efa6cfab35c83
RobertoHille <roberto_hille@estudante.sesisenai.org.br>
2026-03-23 10:10:05 -0300

    add

--- script03.js
+++ script03.js
@@ -7,14 +7,28 @@ let yearloss = 0;
 let difference = 0;
 
 function ganhoanual(){
-while (month <= 12)
+while (month <= 3)
 {
-    let gain = parseFloat(prompt("Digite os ganhos do mês" + month));
-    let loss = parseFloat(prompt("Digite as perdas do mês" + month));
+    let gain = parseFloat(prompt("Digite os ganhos do mês " + month));
+    let loss = parseFloat(prompt("Digite as perdas do mês " + month));
     yeargain += gain;
     yearloss += loss;
     month += 1;
 }
-alert(yeargain);
-alert(yearloss);
+difference = yeargain-yearloss;
+alert("ganhos anuais: " + yeargain);
+alert("Perdas anuais: " + yearloss);
+alert("Resultado: " + difference);
+if (difference > 0)
+{
+    alert("Houve lucro")
+}
+if (difference < 0)
+{
+    alert("Houve prejuízo")
+}
+if (difference = 0)
+{
+    alert("Não soubrou nada")
+}
 }
